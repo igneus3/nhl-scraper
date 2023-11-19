1c6edb92a448b09e9e5289021cd51fcdc63bc2e7
Shane Welsh <shanemwelsh@gmail.com>
2023-11-18 20:49:06 -0500

    Wire up saving games to the db

--- db.py
+++ db.py
@@ -1,3 +1,4 @@
+import json
 import sqlite3
 
 from player import Player
@@ -20,6 +21,30 @@ class NhlRepository:
         self.connection.commit()
 
         cursor.close()
+
+    def __find_one(self, query):
+        cursor = self.connection.cursor()
+
+        records = cursor.execute(query) 
+        result = records.fetchone()
+
+        cursor.close()
+
+        return result
+
+    def insert_game(self, game_id, played):
+        statement = """
+        INSERT INTO games(id, processed) VALUES (?, ?)
+        """
+        params = (game_id, played)
+
+        self.__insert(statement, params)
+
+    def get_game(self, game_id):
+        query = 'SELECT id, processed  FROM GAMES WHERE id = {0}'.format(game_id)
+
+        return self.__find_one(query)
+
 
     def insert_player_game(self, game_id, player):
         statement = """
@@ -99,3 +124,31 @@ class NhlRepository:
 
         self.__insert(statement, params)
 
+
+
+if __name__ == '__main__':
+    player = Player(1)
+
+    player.assists = 1
+    player.blocked_shots = 2
+    player.faceoff_wins = 3
+    player.faceoffs = 4
+    player.giveaways = 5
+    player.goals = 6
+    player.hits_given = 7
+    player.hits_received = 8
+    player.missed_shots = 9
+    player.penalties_committed = 10
+    player.penalties_drawn = 11
+    player.penalties_served = 12
+    player.primary_assists = 13
+    player.saves = 14
+    player.secondary_assists = 15
+    player.shots = 16
+    player.shots_on_goal = 17
+    player.so_failed_shot = 18
+    player.so_shots = 19
+    player.takeaways = 20
+
+    with NhlRepository() as repo:
+        repo.insert_player_game(2023020001, player)
--- main.py
+++ main.py
@@ -7,9 +7,15 @@ def main():
 
     with NhlRepository() as repo:
         while game_processed:
-            print(f'Started processing game: {game_id}')
-            game_processed = process_game(repo, game_id)
-            print(f'Finished processing game: {game_id}')
+            game = repo.get_game(game_id)
+
+            if game is not None and game['processed']:
+                print(f'Skipping {game_id} as it has already been processed.')
+            else:
+                print(f'Started processing game: {game_id}')
+                game_processed = process_game(repo, game_id)
+                print(f'Finished processing game: {game_id}')
+
             game_id += 1
 
     print('\n\nFinished processing all played games')
